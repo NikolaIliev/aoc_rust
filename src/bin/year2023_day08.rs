e9ce0fb984cf6817370a9e8f1404ab4510c8ae8e
Nikola Iliev <nikola.iliev93@gmail.com>
2023-12-08 13:29:26 +0200

    2023/08 solution under 1ms

--- src/bin/year2023_day08.rs
+++ src/bin/year2023_day08.rs
@@ -4,45 +4,57 @@ use aoc_rust::read_input;
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 #[derive(Debug, Default)]
-struct Node {
-    id: String,
-    left: String,
-    right: String,
+struct Node<'a> {
+    id: &'a str,
+    left_id: &'a str,
+    right_id: &'a str,
+    left_idx: usize,
+    right_idx: usize,
 }
 
-fn part_1(input: &str) -> String {
+fn part_1<'a>(input: &'a str) -> String {
     let (instructions_str, nodes_str) = input.split_once("\n\n").unwrap();
     let mut instructions_iter = instructions_str.chars().cycle();
-    let mut nodes: HashMap<String, Node> = HashMap::new();
+    let mut nodes: Vec<Node> = Vec::new();
+    let mut indices: HashMap<&'a str, usize> = HashMap::new();
     let mut iterations = 0;
+    let mut current = 0;
 
     for line in nodes_str.lines() {
-        let id = line[0..3].to_string();
+        let idx = nodes.len();
+        let id = &line[0..3];
+        let left_id = &line[7..10];
+        let right_id = &line[12..15];
 
-        nodes.insert(
-            id.to_owned(),
-            Node {
-                id,
-                left: line[7..10].to_string(),
-                right: line[12..15].to_string(),
-            },
-        );
+        nodes.push(Node {
+            id,
+            left_id,
+            right_id,
+            left_idx: 0,
+            right_idx: 0,
+        });
+        indices.insert(id, idx);
+
+        if id == "AAA" {
+            current = idx;
+        }
     }
 
-    let mut current = nodes.get("AAA").unwrap();
+    for node in nodes.iter_mut() {
+        node.left_idx = *indices.get(node.left_id).unwrap();
+        node.right_idx = *indices.get(node.right_id).unwrap();
+    }
 
-    while current.id != "ZZZ" {
+    while nodes[current].id != "ZZZ" {
         iterations += 1;
 
         let instruction = instructions_iter.next().unwrap();
 
-        current = nodes
-            .get(if instruction == 'L' {
-                &current.left
-            } else {
-                &current.right
-            })
-            .unwrap();
+        current = if instruction == 'L' {
+            nodes[current].left_idx
+        } else {
+            nodes[current].right_idx
+        };
     }
 
     iterations.to_string()
@@ -62,49 +74,57 @@ fn lcm(a: usize, b: usize) -> usize {
     a * b / gcd(a, b)
 }
 
-fn part_2(input: &str) -> String {
+fn part_2<'a>(input: &'a str) -> String {
     let (instructions_str, nodes_str) = input.split_once("\n\n").unwrap();
-    let mut nodes: HashMap<String, Node> = HashMap::new();
-    let mut starting_node_ids: Vec<String> = Vec::new();
+    let mut nodes: Vec<Node> = Vec::new();
+    let mut starting_node_indexes: Vec<usize> = Vec::new();
+    let mut indices: HashMap<&'a str, usize> = HashMap::new();
 
     for line in nodes_str.lines() {
-        let id = line[0..3].to_string();
-        let id_last_ch = id.chars().nth(2).unwrap();
-        let node = Node {
-            id: line[0..3].to_string(),
-            left: line[7..10].to_string(),
-            right: line[12..15].to_string(),
-        };
+        let idx = nodes.len();
+        let id = &line[0..3];
+        let left_id = &line[7..10];
+        let right_id = &line[12..15];
 
-        nodes.insert(node.id.to_owned(), node);
+        nodes.push(Node {
+            id,
+            left_id,
+            right_id,
+            left_idx: 0,
+            right_idx: 0,
+        });
+        indices.insert(id, idx);
 
-        if id_last_ch == 'A' {
-            starting_node_ids.push(id.to_owned());
+        if &id[2..] == "A" {
+            starting_node_indexes.push(idx);
         }
     }
 
-    starting_node_ids
+    for node in nodes.iter_mut() {
+        node.left_idx = *indices.get(node.left_id).unwrap();
+        node.right_idx = *indices.get(node.right_id).unwrap();
+    }
+
+    starting_node_indexes
         .par_iter()
-        .map(|id| {
+        .map(|&idx| {
             let mut instructions_iter = instructions_str.chars().cycle();
             let mut cycle_start = 0;
             let mut cycle_end = 0;
 
-            let mut current = nodes.get(id).unwrap();
+            let mut current = idx;
 
             loop {
                 cycle_end += 1;
                 let instruction = instructions_iter.next().unwrap();
 
-                current = nodes
-                    .get(if instruction == 'L' {
-                        &current.left
-                    } else {
-                        &current.right
-                    })
-                    .unwrap();
+                current = if instruction == 'L' {
+                    nodes[current].left_idx
+                } else {
+                    nodes[current].right_idx
+                };
 
-                let last_char = *current.id.as_bytes().last().unwrap() as char;
+                let last_char = *nodes[current].id.as_bytes().last().unwrap() as char;
 
                 if last_char == 'Z' {
                     if cycle_start == 0 {
